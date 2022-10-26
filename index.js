0d926b841b1d49ed5a44a8ac30baa6d1be3035d9
DANCAN-OPENJI-DEVELOPS <114863301+DANCAN-OPENJI-DEVELOPS@users.noreply.github.com>
2022-10-26 11:52:10 +0300

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-# BINARY-AGENT
-
-Return an English translated sentence of the passed binary string. The binary string will be space separated.
-Intermediate Algorithm Scripting.
-
-## local development
-
-Open index.js file in your browser console.
